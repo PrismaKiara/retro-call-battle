64b46e09d383e4bfb616c105295cf4652d754fd8
PrismaKiara <l.tomasch@gmail.com>
2025-05-12 14:52:26 +0200

    Update admin.js

--- pages/admin.js
+++ pages/admin.js
@@ -1,1 +1,8 @@
-// Admin Dashboard page
+export default function Admin() {
+  return (
+    <div>
+      <h1>Admin-Dashboard</h1>
+      <p>Hier siehst du bald alle Battle-Daten!</p>
+    </div>
+  );
+}
